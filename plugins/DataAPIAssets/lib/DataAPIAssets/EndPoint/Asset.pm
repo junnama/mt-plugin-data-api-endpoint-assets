a063c10a28c61b22af21bbd7ec8de1792389421b
Junnama Noda <junnama@alfasado.jp>
2014-06-01 17:28:18 +0900

    Add Query Parameters.

--- plugins/DataAPIAssets/lib/DataAPIAssets/EndPoint/Asset.pm
+++ plugins/DataAPIAssets/lib/DataAPIAssets/EndPoint/Asset.pm
@@ -17,7 +17,36 @@ sub list_assets {
     my $class = $paths[ scalar( @paths ) - 1 ];
     $class =~ s/s$//;
     run_permission_filter( $app, 'data_api_list_permission_filter', 'asset' ) or return;
-    my $iter = MT->model( $class )->load_iter( { blog_id => $blog->id } );
+    # sortBy ( created_on | modified_on | id | label )
+    # sortOrder ( descend | ascend )
+    # limit offset
+    my $args;
+    my $sort_order = 'descend';
+    my $sort_by = 'id';
+    my @sort_col = qw( created_on modified_on id label );
+    if ( my $sortBy = $app->param( 'sortBy' ) ) {
+        if ( grep( /^$sortBy$/, @sort_col ) ) {
+            $sort_by = $sortBy;
+        }
+    }
+    if ( my $sortOrder = $app->param( 'sortOrder' ) ) {
+        if ( $sortOrder eq 'ascend' ) {
+            $sort_order = $sortOrder;
+        }
+    }
+    $args->{ sort_by } = $sort_by;
+    $args->{ direction } = $sort_order;
+    my $limit = 10;
+    if ( $app->param( 'limit' ) ) {
+        $limit = $app->param( 'limit' ) + 0;
+    }
+    my $offset = 0;
+    if ( $app->param( 'offset' ) ) {
+        $offset = $app->param( 'offset' ) + 0;
+    }
+    $args->{ limit } = $limit;
+    $args->{ offset } = $offset;
+    my $iter = MT->model( $class )->load_iter( { blog_id => $blog->id }, $args );
     my @assets;
     while ( my $asset = $iter->() ) {
         push( @assets, $asset );
