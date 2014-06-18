559c58b1f0ac8477f08b8e3647434df7fa3907a6
Junnama Noda <junnama@alfasado.jp>
2014-06-18 12:01:04 +0900

    Add Endpoints. update_asset and delete_asset.

--- plugins/DataAPIAssets/lib/DataAPIAssets/EndPoint/Asset.pm
+++ plugins/DataAPIAssets/lib/DataAPIAssets/EndPoint/Asset.pm
@@ -16,6 +16,103 @@ sub get_asset {
     return $asset;
 }
 
+sub update_asset {
+    my ( $app, $endpoint ) = @_;
+    my ( $blog, $asset ) = context_objects( @_ ) or return;
+    if (! $app->user || $app->user->is_anonymous ) {
+        return $app->print_error( 'Unauthorized', 401 );
+    } elsif (! $app->can_do( 'edit_assets' ) ) {
+        return $app->print_error( 'Permission denied.', 401 );
+    }
+    if ( my $method = $app->param( '__method' ) ) {
+        if ( lc( $method ) eq 'delete' ) {
+            return delete_asset( @_ );
+        }
+    }
+    my ( $fh, $info ) = $app->upload_info( 'file' );
+    my $new_obj;
+    if ( $fh && (! $app->param( 'asset' ) ) ) {
+        $new_obj = $asset;
+    } else {
+        $new_obj = $app->resource_object( 'asset', $asset ) or return;
+    }
+    my $tmp_id;
+    if ( $fh ) {
+        return $app->error( 403 ) unless $app->can_do( 'upload' );
+        my $tmp_file = $asset->file_path;
+        require MT::FileMgr;
+        my $fmgr = MT::FileMgr->new( 'Local' ) or die MT::FileMgr->errstr;
+        if ( $fmgr->exists( $tmp_file ) ) {
+            $fmgr->rename( $tmp_file, "${tmp_file}.tmp" );
+        }
+        $app->param( 'site_path', 1 );
+        my %keys =( overwrite => 'overwrite_yes',
+                    fileName => 'fname',
+                    temp => 'temp',
+                    path => 'extra_path',
+                    autoRenameIfExists => 'auto_rename_if_exists',
+                    normalizeOrientation => 'normalize_orientation',
+                  );
+        for my $key ( keys %keys ) {
+            if ( my $value = $app->param( $key ) ) {
+                $app->param( $keys{ $key }, $value );
+            }
+        }
+        my ( $upload_asset, $bytes ) = MT::CMS::Asset::_upload_file( $app, @_ );
+        if (! $upload_asset ) {
+            $fmgr->rename( "${tmp_file}.tmp", $tmp_file );
+            return $app->error( 500 );
+        } else {
+            $fmgr->delete( "${tmp_file}.tmp" );
+            $tmp_id = $upload_asset->id;
+            $upload_asset->id( $asset->id );
+            $upload_asset->clear_cache();
+            if ( $app->param( 'asset' ) ) {
+                $new_obj = $app->resource_object( 'asset', $upload_asset ) or return;
+            } else {
+                $new_obj = $upload_asset;
+                my $cols = $asset->column_names;
+                my @not_cp = qw( file_path mime_type url class );
+                for my $col ( @$cols ) {
+                    if (! grep( /^$col$/, @not_cp ) ) {
+                        if ( $new_obj->has_column( $col ) ) {
+                            $new_obj->$col( $asset->$col );
+                        }
+                    }
+                }
+            }
+            $new_obj->clear_cache();
+        }
+    }
+    save_object(
+        $app, 'asset',
+        $new_obj,
+        $asset,
+        sub {
+            $new_obj->modified_by( $app->user->id );
+            $_[ 0 ]->();
+        }
+    ) or return;
+    if ( $tmp_id ) {
+        if ( my $upload = MT->model( 'asset' )->load( $tmp_id ) ) {
+            $upload->remove or return $app->error( 500 );
+        }
+    }
+    return $new_obj;
+}
+
+sub delete_asset {
+    my ( $app, $endpoint ) = @_;
+    my ( $blog, $asset ) = context_objects( @_ ) or return;
+    if (! $app->user || $app->user->is_anonymous ) {
+        return $app->print_error( 'Unauthorized', 401 );
+    } elsif (! $app->can_do( 'edit_assets' ) ) {
+        return $app->print_error( 'Permission denied.', 401 );
+    }
+    remove_object( $app, 'asset', $asset ) or return;
+    return $asset;
+}
+
 sub list_assets {
     my ( $app, $endpoint ) = @_;
     my ( $blog ) = context_objects( @_ ) or return;
